ad28fb322173ed05f30fa6f6f623deb4d57806cf
Garrett Lander <garrett.jlander@gmail.com>
2024-09-28 17:18:39 +0200

    webhook syncs to database

--- backend/src/controllers/webhooks.ts
+++ backend/src/controllers/webhooks.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express-serve-static-core';
 import { Webhook } from 'svix';
 
+import User from '../models/User.ts';
 import ErrorResponse from '../utils/ErrorResponse.ts';
+
 interface WebhookEvent {
     data: any;
     type: string;
@@ -9,9 +11,6 @@ interface WebhookEvent {
 
 const clerkWebhook = async (req: Request, res: Response): Promise<void> => {
     try {
-        // const payloadString = req.body.toString();
-        // const svixHeaders = req.headers;
-
         if (!process.env.WEBHOOK_SECRET)
             throw new ErrorResponse(
                 'Webhook key is missing, you are unauthorized.',
@@ -42,51 +41,27 @@ const clerkWebhook = async (req: Request, res: Response): Promise<void> => {
         const { id, ...info } = evt.data;
         // Handle the webhooks
         const eventType = evt.type;
-        console.log('webHook event type:', eventType);
-        // console.log(info);
-        if (eventType === 'user.created') {
-            console.log(`User ${id} was ${eventType}`);
+        // console.log('webHook event type:', eventType);
 
+        let message = 'Nothing happened';
+
+        if (eventType === 'user.created') {
+            // console.log(`User ${id} was ${eventType}`);
+
+            const email = info.email_addresses[0].email_address;
+            const username = info.username;
             const firstName = info.first_name;
             const lastName = info.last_name;
-            const username = info.username;
 
-            // await ClerkUser.create({
-            //     clerkUserId: id,
-            //     firstName,
-            //     lastName,
-            //     username,
-            //     previewPage: {
-            //         pages: {
-            //             home: {
-            //                 navBar: [{ menuItem: 'Change Me' }],
-            //                 hero: [
-            //                     {
-            //                         imgUrl: 'https://nextui.org/images/fruit-8.jpeg',
-            //                         title: 'Add a title here',
-            //                         text: 'And a bit of descriptive text here...',
-            //                         button: 'Click Me',
-            //                     },
-            //                 ],
-            //                 cards: [
-            //                     {
-            //                         imgUrl: 'https://hips.hearstapps.com/hmg-prod/images/crepes-index-64347419e3c7a.jpg',
-            //                         title: 'A delicious recipe here',
-            //                         text: 'Simply delicious',
-            //                         button: 'To Recipe',
-            //                     },
-            //                 ],
-            //                 footer: [
-            //                     {
-            //                         footerItem:
-            //                             'You can customize the footer too.',
-            //                     },
-            //                 ],
-            //             },
-            //         },
-            //     },
-            // });
-            console.log(`${username} saved to database`);
+            await User.create({
+                clerkUserId: id,
+                email,
+                username,
+                firstName,
+                lastName,
+            });
+
+            message = `${username} saved to database`;
             // const newClerkUser = await ClerkUser.find({
             //     clerkUserId: id,
             // });
@@ -132,30 +107,33 @@ const clerkWebhook = async (req: Request, res: Response): Promise<void> => {
         if (eventType === 'user.updated') {
             console.log(`User ${id} was ${eventType}`);
 
+            const email = info.email_addresses[0].email_address;
+            const username = info.username;
             const firstName = info.first_name;
             const lastName = info.last_name;
-            const username = info.username;
 
-            // await ClerkUser.findOneAndUpdate(
-            //     { clerkUserId: id },
-            //     {
-            //         firstName,
-            //         lastName,
-            //         username,
-            //     }
-            // );
-            console.log(`${username} updated successfully`);
+            await User.findOneAndUpdate(
+                { clerkUserId: id },
+                {
+                    email,
+                    username,
+                    firstName,
+                    lastName,
+                }
+            );
+            message = `${username} updated successfully`;
         }
         if (eventType === 'user.deleted') {
             console.log(`User ${id} was ${eventType}`);
 
-            // await ClerkUser.findOneAndDelete({ clerkUserId: id });
+            await User.findOneAndDelete({ clerkUserId: id });
             // await BlogModel.findOneAndDelete({ clerkUserId: id });
-            console.log(`User deleted successfully`);
+            message = `User deleted successfully`;
         }
+
         res.status(200).json({
             success: true,
-            message: 'Webhook received',
+            message: message,
         });
     } catch (err) {
         console.log(err);
---
+++ backend/src/models/User.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema(
+    {
+        clerkUserId: {
+            type: String,
+            unique: true,
+            required: [true, 'Clerk user ID is required'],
+        },
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            unique: true,
+        },
+        username: {
+            type: String,
+            unique: true,
+        },
+        firstName: String,
+        lastName: String,
+    },
+    { timestamps: true }
+);
+
+export default model('User', userSchema);
